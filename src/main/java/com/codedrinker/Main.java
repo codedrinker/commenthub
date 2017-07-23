1d04a9ba975e2d81b043ab3ee7da9749f2f96fc6
codedrinker <gwangchunlei@gmail.com>
2017-07-23 22:30:40 +0800

    add main function for Main class

--- src/main/java/com/codedrinker/Main.java
+++ src/main/java/com/codedrinker/Main.java
@@ -1,6 +1,7 @@
 
 package com.codedrinker;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -14,6 +15,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Configuration
 @ImportResource({"classpath*:applicationContext.xml"})
 public class Main {
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Main.class, args);
+    }
+
     @RequestMapping("/")
     String index() {
         return "index";
