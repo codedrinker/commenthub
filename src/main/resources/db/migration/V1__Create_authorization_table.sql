fc6460239cc1155303f75df5d46ddb830a583dee
codedrinker <gwangchunlei@gmail.com>
2017-07-29 16:44:38 +0800

    try to fix migration again

--- src/main/resources/db/migration/V1__Create_authorization_table.sql
+++ src/main/resources/db/migration/V1__Create_authorization_table.sql
@@ -1,8 +1,8 @@
-CREATE TABLE `t_authorization`
+CREATE TABLE t_authorization
 (
-  `id` BIGINT(13) PRIMARY KEY NOT NULL,
-  `token` VARCHAR(40) NOT NULL,
-  `email` VARCHAR(100),
-  `utime` BIGINT(13) DEFAULT '0',
-  `ctime` BIGINT(13) DEFAULT '0'
+  id BIGINT(13) PRIMARY KEY NOT NULL,
+  token VARCHAR(40) NOT NULL,
+  email VARCHAR(100),
+  utime BIGINT(13) DEFAULT '0',
+  ctime BIGINT(13) DEFAULT '0'
 );
