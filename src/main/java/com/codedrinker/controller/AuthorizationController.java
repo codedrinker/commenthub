c3df6755ba3a026fe8e555800540dddfea5b7875
codedrinker <gwangchunlei@gmail.com>
2017-08-05 08:53:33 +0800

    add log

--- src/main/java/com/codedrinker/controller/AuthorizationController.java
+++ src/main/java/com/codedrinker/controller/AuthorizationController.java
@@ -3,6 +3,8 @@ package com.codedrinker.controller;
 import com.codedrinker.entity.Authorization;
 import com.codedrinker.entity.ResponseDTO;
 import com.codedrinker.service.AuthorizationService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,30 +14,35 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping(value = "authorizations")
 public class AuthorizationController {
+    private final Logger logger = LoggerFactory.getLogger(AuthorizationController.class);
 
     @Autowired
     private AuthorizationService authorizationService;
 
     @RequestMapping(method = RequestMethod.POST)
     Object create(@RequestBody Authorization authorization) {
+        logger.info("save authorization ->{}", authorization);
         ResponseDTO responseDTO = authorizationService.save(authorization);
         return responseDTO;
     }
 
     @RequestMapping(method = RequestMethod.PUT)
     Object update(@RequestBody Authorization authorization) {
+        logger.info("update by authorization ->{}", authorization);
         ResponseDTO responseDTO = authorizationService.update(authorization);
         return responseDTO;
     }
 
     @RequestMapping(method = RequestMethod.DELETE, value = "/{id}")
     Object delete(@PathVariable(value = "id") Integer id) {
+        logger.info("delete by id ->{}", id);
         authorizationService.delete(id);
         return ResponseDTO.ok(null);
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "/{id}")
     Object get(@PathVariable(value = "id") Integer id) {
+        logger.info("get by id ->{}", id);
         ResponseDTO responseDTO = authorizationService.get(id);
         return responseDTO;
     }
