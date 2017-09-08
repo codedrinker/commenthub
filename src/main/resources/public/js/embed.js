435bcc693afe891f6154b3c41c7459f9a50e104d
codedrinker <gwangchunlei@gmail.com>
2017-09-08 21:40:42 +0800

    dynamic set iframe height by body height

--- src/main/resources/public/js/embed.js
+++ src/main/resources/public/js/embed.js
@@ -6,7 +6,7 @@ var api = "https://api.github.com/repos/commenthub/commenthub/issues";
 // https://api.github.com/repos/commenthub/commenthub/issues?labels=http://www.majiang.life,http://www.majiang.life/blog/simple-in-memory-cache-in-java/
 //https://github.com/commenthub/commenthub/issues/1
 function init() {
-    $("#commenthub_thread").html("<iframe style='width: 100%;height: 100%;border: none;' src='https://commenthub.herokuapp.com/comments?commenthub_website=" + commenthub_website + "&commenthub_identifier=" + commenthub_identifier + "'></iframe>");
+    $("#commenthub_thread").html("<iframe id='commenthub_iframe' style='width: 1px !important; min-width:100% !important; border: none !important; overflow: hidden !important;' src='https://commenthub.herokuapp.com/comments?commenthub_website=" + commenthub_website + "&commenthub_identifier=" + commenthub_identifier + "'></iframe>");
 }
 
 if (typeof jQuery == 'undefined') {
